aa954223d171d21e9b94328c41948f3aef2d0058
numa123 <104247745+numa123@users.noreply.github.com>
2024-09-26 17:07:00 +0900

    Add a tokenizer to allow space characters between tokens

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,50 @@
+#[derive(Debug)]
+enum TokenKind {
+    TK_PUNCT,
+    TK_NUM,
+}
+
+#[derive(Debug)]
+struct Token {
+    kind: TokenKind,
+    val: i32,
+    str: String,
+}
+
+fn tokenize(p: &mut &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    while !p.is_empty() {
+        let c = p.chars().next().unwrap();
+
+        if c == ' ' {
+            *p = &p[1..];
+            continue;
+        }
+        if c.is_digit(10) {
+            let num = parse_number(p);
+            tokens.push(Token {
+                kind: TokenKind::TK_NUM,
+                val: num.parse().unwrap(),
+                str: num,
+            });
+            continue;
+        }
+        if c == '+' || c == '-' {
+            tokens.push(Token {
+                kind: TokenKind::TK_PUNCT,
+                val: 0,
+                str: p[0..1].to_string(),
+            });
+            *p = &p[1..];
+            continue;
+        }
+    }
+    return tokens;
+}
+
 fn parse_number(p: &mut &str) -> String {
     let num: String = p.chars().take_while(|c| c.is_digit(10)).collect();
-    *p = &p[num.len()..];
+    *p = &p[num.len()..]; // これは関数の外に出した方が明示的に書きやすいかも？
     return num;
 }
 
@@ -11,26 +55,29 @@ fn main() {
         std::process::exit(1);
     }
 
-    // 数字の後に+が来るか確認する。まずは一桁+一桁のみ
     let mut input = &args[1][..];
+    let mut tokens = tokenize(&mut input);
 
-    // 最初の数値をx0に入れる
+    // 最初の文字は数値
     println!(".global _main");
     println!("_main:");
-    println!("  mov x0, {}", parse_number(&mut input));
+    println!("  mov x0, {}", tokens[0].val);
+    tokens.remove(0); // 1つ配列を消費
 
-    while !input.is_empty() {
-        if input[0..1].eq("+") {
-            input = &input[1..];
-            println!("  add x0, x0, {}", parse_number(&mut input));
+    while !tokens.is_empty() {
+        if tokens[0].str.eq("+") {
+            tokens.remove(0);
+            println!("  add x0, x0, {}", tokens[0].val);
+            tokens.remove(0);
             continue;
         }
-        if input[0..1].eq("-") {
-            input = &input[1..];
-            println!("  sub x0, x0, {}", parse_number(&mut input));
+        if tokens[0].str.eq("-") {
+            tokens.remove(0);
+            println!("  sub x0, x0, {}", tokens[0].val);
+            tokens.remove(0);
             continue;
         }
-        eprintln!("invalid input: {}", input[0..1].to_string());
+        eprintln!("invalid input: {}", tokens[0].str);
         std::process::exit(1);
     }
 
