0e238fd8c753fe850d84a057f985ccb00aafdb4d
numa123 <104247745+numa123@users.noreply.github.com>
2024-10-05 19:19:10 +0900

    Compile an integer to an executable that exits with the given number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("{}: invalid number of arguments", args[0]);
+        panic!();
+    }
+    let input = &args[1];
+
+    println!(".global _main");
+    println!("_main:");
+    println!("    mov w0, {}", input);
+    println!("    ret");
 }
